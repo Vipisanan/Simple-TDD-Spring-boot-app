fcdfde6f464897dd4da35f1dbdd716ec1ff6e850
vipiBitbucket <vipivipisanan@gmail.com>
2025-02-23 10:49:35 +0530

    init commit

---
+++ src/main/java/com/vp/simpleTaskTddApp/SimpleTaskTddAppApplication.java
@@ -0,0 +1,13 @@
+package com.vp.simpleTaskTddApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTaskTddAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTaskTddAppApplication.class, args);
+	}
+
+}
